b95415ce25a17ce8ccc939d031b3909fa72f04f3
Ramachandra Maddela <mramachandra9@gmail.com>
2023-02-18 11:24:04 +0530

    code

---
+++ src/main/java/com/cicd/actions/GithubCicdActionsApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cicd/actions/controller/CicdController.java
@@ -0,0 +1,17 @@
+package com.cicd.actions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cicd")
+public class CicdController {
+	
+@GetMapping("/message")
+public String message() {
+	return "CICD demo message!";
+	
+}
+
+}
